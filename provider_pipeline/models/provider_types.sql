d7cf0b97ce97e362d8438309b685ec5535cb0018
Muhil <muhil.arasu@citiustech.com>
2025-02-28 19:11:00 +0530

    Solution for Provider Degrees - provider_types

---
+++ provider_pipeline/models/provider_types.sql
@@ -0,0 +1,51 @@
+with providers as (
+    select * from {{ ref('providers') }}
+),
+
+degree_types as (
+    select * from {{ ref('degree_types') }}
+),
+
+-- split degrees column into an array and join with degree_types
+exploded_degrees as (
+    select 
+        p.customer_id,
+        p.first_name,
+        p.last_name,
+        trim(value) as degree
+    from providers p
+    cross join unnest(string_split(p.degrees, ',')) as value
+),
+
+-- join with degree_types to get rank
+degrees_with_rank as (
+    select 
+        ed.customer_id,
+        ed.first_name,
+        ed.last_name,
+        ed.degree,
+        dt.ptui,
+        dt.rank
+    from exploded_degrees ed
+    left join degree_types dt on ed.degree = dt.degree_name
+),
+
+-- get the lowest rank degree per provider
+ranked_degrees as (
+    select 
+        customer_id,
+        first_name,
+        last_name,
+        ptui,
+        rank() over (partition by customer_id order by rank asc) as rnk
+    from degrees_with_rank
+)
+
+-- select only the lowest ranked degree per provider
+select 
+    customer_id,
+    first_name,
+    last_name,
+    ptui
+from ranked_degrees
+where rnk = 1;
